f000d862a5d239bdb08e4e4b87bf529752388b1d
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2022-08-14 19:23:28 +0200

    projet fini

---
+++ src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque natus
+            inventore molestiae iste aliquid quasi non iure repellat recusandae
+            dolorum eum, numquam distinctio laborum facere voluptas pariatur officia
+            voluptate dignissimos cupiditate modi neque nulla enim. Ipsam, ad, animi
+            voluptatibus nam sapiente similique asperiores pariatur, reprehenderit
+            delectus vitae nostrum eaque praesentium quaerat excepturi nisi fugit
+            autem totam. Debitis dolores ab dolor est, voluptatum totam illum rem
+            aperiam officiis assumenda ducimus aliquam in distinctio eius eligendi nam
+            eum. Dolores officiis minima id odio at, delectus numquam sapiente? Libero
+            quisquam voluptate neque magnam, provident id. Assumenda, temporibus illum
+            magnam vitae aperiam facere quisquam! Inventore eaque nam non? Expedita
+            molestias magnam sunt labore est nostrum iure amet tenetur fugiat, a eius
+            eos quam, voluptatibus incidunt. Quae quos, minima quidem consequatur
+            architecto earum corrupti. Sequi, accusantium! Suscipit dicta similique,
+            laboriosam eos consectetur amet dignissimos aspernatur, eius reiciendis
+            nisi accusantium quaerat ea quo consequatur odit nobis quidem beatae
+            dolorem, sapiente a ad laborum expedita voluptatem ex! Veritatis a labore
+            dolore enim magni, rem vero perspiciatis commodi, incidunt excepturi amet
+            voluptatum deserunt tempora aliquam consequuntur? Mollitia qui non
+            corporis error sint beatae saepe vitae aliquam, sequi quod accusamus porro
+            et officiis reprehenderit cumque laboriosam dolorum animi nisi?
+            <br />
+            <br />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic veniam
+            laudantium distinctio, quo adipisci mollitia maxime modi aut asperiores
+            totam quod ad molestias animi maiores. Officia placeat quasi quod itaque,
+            ipsa voluptatibus, voluptate facere dignissimos nam quo maxime rerum
+            molestias inventore ipsam, iusto animi ut? Nemo molestiae perferendis id
+            commodi?
+        </div>
+    );
+};
+
+export default About;
