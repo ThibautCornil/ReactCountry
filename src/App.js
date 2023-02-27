e7510747a057fdd13b5f753413742ce56103ffdc
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2023-02-27 10:34:28 +0100

    test mise en ligne

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/ReactCountry/' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
         <Route path='*' element={<Home />} />
